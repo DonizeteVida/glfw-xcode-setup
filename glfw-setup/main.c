3fccd404d7282a7e59f39c7a9224d3c919d9d27e
DonizeteVida <donizetevida1998@outlook.com>
2024-02-18 18:38:06 -0300

    Initial Commit

---
+++ glfw-setup/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  glfw-setup
+//
+//  Created by Donizete Vida on 18/02/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
